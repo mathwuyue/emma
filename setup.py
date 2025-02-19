afb0c8e9cb2fb238b43181a6c5d7f86be998242d
Yue Wu <wuyue681@gmail.com>
2025-02-19 23:31:57 +0800

    Refactor: update calorie calculation in exercise summary and increment version to 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="emma",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     install_requires=[
         "capybara>=0.1.0",
