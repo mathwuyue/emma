f928a06d6483c222a53695f27fe8e2503117c60b
Yue Wu <wuyue681@gmail.com>
2025-07-13 10:53:56 +0800

    Feat: Add report check feature

--- emma/care/__init__.py
+++ emma/care/__init__.py
@@ -1,2 +1,2 @@
-from .physicalexam import exam_report_ocr, generate_physicalexam_list
+from .physicalexam import ReportAnalysis, generate_physicalexam_list
 from .record import cal_exp_weight
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="emma",
-    version="0.7.1",
+    version="0.8.0",
     packages=find_packages(),
     install_requires=[
         "capybara>=0.2.0",
